8800c7e993cc84c3d3776d63df5f4983a1860c83
test <damianlukawski11@gmail.com>
2020-06-08 14:32:03 +0200

    using group by function and plotting

--- new.py
+++ new.py
@@ -1,4 +1,22 @@
 import numpy as np
+import pandas as pd
+import openpyxl
+import matplotlib.pyplot as plt
 
-a= np.arange(0,1,0.1)
-print(a)
+
+plt.close('all')
+#x = input("Please provide the filename and path ")
+pd.set_option( "display.max_columns", None)
+#pokemon_pandas = pd.read_csv('{}'.format(x))
+pp = pd.read_csv('pokemon.csv') #pp - pokemon pandas
+#pp.set_option("display.max_rows", max_rows, "display.max_columns", max_cols)
+#print(pokemon_pandas)
+#print(pp.columns)
+#print(pp.loc[pp['Type 1'] == "Fire"])
+
+group = pp.groupby(['Type 1']).count()
+print(group["#"])
+
+plt.figure()
+group["#"].plot(kind='bar', color='green')
+plt.show()
